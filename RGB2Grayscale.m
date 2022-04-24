9031855278d5fb01a57ebf0fe09b73a94a32202c
yohanachristella <103626934+yohanachristella@users.noreply.github.com>
2022-04-24 22:41:52 +0700

    Update RGB2Grayscale.m

--- RGB2Grayscale.m
+++ RGB2Grayscale.m
@@ -1,13 +1,13 @@
-g = imread("Tulips.jpg")
-figure;imshow(g);
-size(g)
+g = imread("Tulips.jpg") %memanggil gambar tulips
+figure;imshow(g); %menampilkan gambar tulips
+size(g) %menampilkan ukuran file dari gambar tulips
 
 %Mengubah RGB image menjadi Grayscale image 
-K = rgb2gray(g);
+K = rgb2gray(g); %Konversi dari RGB ke Grayscale disimpan ke dalam variabel K
 
-subplot(1,2,1);imshow(g)
-subplot(1,2,2);imshow(K)
+subplot(1,2,1);imshow(g) %menampilkan gambar asli yang disimpan ke dalam variabel g di sebelah kiri
+subplot(1,2,2);imshow(K) %menampilkan gambar hasil konversi yang disimpan ke dalam variabel K di sebelah kanan
 
-%figure;imshow(K);
+%figure;imshow(K); 
 %figure;imhist(K); %menampilkan histogram image 
 size(K)
