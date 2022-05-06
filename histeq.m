95befe8bf94edde7b8af4becc8625989a0779167
yohanachristella <103626934+yohanachristella@users.noreply.github.com>
2022-05-07 01:04:31 +0700

    Update histeq.m

--- histeq.m
+++ histeq.m
@@ -1,7 +1,6 @@
 i = imread('einstein.jpg'); %Memanggil file gambar einstein
 
-imh = imadjust(i,[0.3,0.6],[0.0,1.0]); %Mengadjust kontras
-%imh1 = histeq(i);
+imh = histeq(i); %Variabel untuk pemerataan histogram
 figure; %Menampilkan hasil pengolahan citra
 subplot(2,2,3); imshow(imh); title('Hist-Eq'); %Menampilkan hasil histeq
 subplot(2,2,4); imhist(imh); %Menampilkan equalization histeq
