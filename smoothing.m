b38bd91d6810a89fc4d7fbf0d9045ae82ce42fb7
yohanachristella <103626934+yohanachristella@users.noreply.github.com>
2022-05-07 12:48:08 +0700

    Create smoothing.m

---
+++ smoothing.m
@@ -0,0 +1,4 @@
+%Lowpass filter
+n = imsmooth(g, 'gaussian', 10); %Fungsi untuk memperhalus/memblurkan citra
+subplot(1,2,1); imshow(g); %Menampilkan gambar asli
+subplot(1,2,2); imshow(n); %Menampilkan gambar hasil smooth lowpass filter
